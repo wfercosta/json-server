97fd05a11766f81a8dc75377a97fb86a0e3b946d
Guilherme Rodrigues Bueno <guilhermerb@ciandt.com>
2020-11-25 14:02:45 -0300

    Adding async await

--- encryptionMethods.js
+++ encryptionMethods.js
@@ -1,8 +1,8 @@
 const jose = require('node-jose');
 
 module.exports = {
-  getSymetricKey() {
-    const key64 = Buffer.from(process.env.KEY_OUATH_IRIS).toString('base64');
+  async getSymetricKey() {
+    const key64 = Buffer.from(process.env.KEY_OAUTH_IRIS).toString('base64');
     return jose.JWK.asKey({
       kid: 'default',
       alg: 'A256GCM',
@@ -10,8 +10,9 @@ module.exports = {
       k: key64,
     }).then((result) => result);
   },
-  decryptSymetric(encoded) {
-    const symetricKey = this.getSymetricKey();
+  async decryptSymetric(encoded) {
+    const symetricKey = await this.getSymetricKey();
+    // console.log(`Symetric key ${JSON.stringify(symetricKey)}`);
     return jose.JWE.createDecrypt(symetricKey)
       .decrypt(encoded)
       .then((result) => result.plaintext.toString('utf8'));
--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const port = 3000;
 
 const DEFAULT_KEY = 'default';
 
-const getObjectKey = (token) => {
-  const decryptedInfo = encryptionMethods.decryptSymetric(token);
+const getObjectKey = async (token) => {
+  const decryptedInfo = await encryptionMethods.decryptSymetric(token);
   const payload = JSON.parse(decryptedInfo);
 
   return payload[process.env.ENCRYPTED_KEY] || DEFAULT_KEY;
 };
 
-const buildResponse = (collection, objectKey) => {
+const buildResponse = async (collection, objectKey) => {
   const responseKey = db.get(collection).get(objectKey).value()
     || db.get(collection).get(DEFAULT_KEY).value();
 
@@ -27,15 +27,15 @@ const buildResponse = (collection, objectKey) => {
 Object.keys(routes).forEach((key) => {
   const { method, collection } = routes[key];
   console.log(`\nRegistering route '${key}' with method '${method}' -> '${collection}'`);
-  server[method](key, (req, res, next) => {
+  server[method](key, async (req, res, next) => {
     console.log(`\nNew request arrived with key ${key} and headers ${JSON.stringify(req.headers)}`);
-    const token = req.headers.Authorization;
+    const token = req.headers.authorization;
 
     console.log(`\nToken value ${token}`);
     const objectKey = getObjectKey(token);
 
-    console.log(`\nObject key ${objectKey}`);
-    const response = buildResponse(collection, objectKey);
+    console.log(`\nObject key ${JSON.stringify(objectKey)}`);
+    const response = await buildResponse(collection, objectKey);
 
     console.log(`\nAccess to route ${key}`);
     res.jsonp(response);
