966cfa5306e601fb549eca9d6a58ebb3a5927d18
Guilherme Rodrigues Bueno <guilhermerb@ciandt.com>
2020-11-25 07:21:32 -0300

    Fixing module export

--- encryptionMethods.js
+++ encryptionMethods.js
@@ -1,7 +1,7 @@
 const jose = require('node-jose');
 
-export default class EncryptionMethods {
-  static getSymetricKey() {
+module.exports = {
+  getSymetricKey() {
     const key64 = Buffer.from(process.env.KEY_OUATH_IRIS).toString('base64');
     return jose.JWK.asKey({
       kid: 'default',
@@ -9,12 +9,11 @@ export default class EncryptionMethods {
       kty: 'oct',
       k: key64,
     }).then((result) => result);
-  }
-
-  static decryptSymetric(encoded) {
+  },
+  decryptSymetric(encoded) {
     const symetricKey = this.getSymetricKey();
     return jose.JWE.createDecrypt(symetricKey)
       .decrypt(encoded)
       .then((result) => result.plaintext.toString('utf8'));
-  }
-}
+  },
+};
